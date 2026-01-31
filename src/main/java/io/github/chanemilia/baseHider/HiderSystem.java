210c997d37e7ebf9dbb8b2370f2884c303e2a251
Emilia <ecsr08@outlook.com>
2026-01-31 22:50:18 +0000

    removed whitespace

--- src/main/java/io/github/chanemilia/baseHider/HiderSystem.java
+++ src/main/java/io/github/chanemilia/baseHider/HiderSystem.java
@@ -395,7 +395,6 @@ public class HiderSystem implements Listener {
         WrappedBlockData replacement = WrappedBlockData.createData(config.replacementBlock);
 
         int i = 0;
-
         for (int y = 0; y < 16; y++) {
             for (int z = 0; z < 16; z++) {
                 for (int x = 0; x < 16; x++) {
