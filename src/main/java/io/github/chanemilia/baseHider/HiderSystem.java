a43e38e8af908c344cb92e13a28fe9c83580d28b
Emilia <ecsr08@outlook.com>
2026-02-01 12:42:26 +0000

    Dead code cleanup

--- src/main/java/io/github/chanemilia/baseHider/HiderSystem.java
+++ src/main/java/io/github/chanemilia/baseHider/HiderSystem.java
@@ -59,7 +59,6 @@ public class HiderSystem implements Listener {
         registerChunkListener();
 
         startQueueProcessor();
-        startCacheCleaner();
         startRescanTask();
     }
 
